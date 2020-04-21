60a33bb029a46aae9b24fdc56c93bd7bcb6e5dbf
matmcf <63942762+matmcf@users.noreply.github.com>
2020-04-21 20:32:04 +1200

    Update ecwid.js
    Removed sample code

--- ecwid.js
+++ ecwid.js
@@ -5,38 +5,7 @@
  * order is more than $99
  */
 
-var promoMessage = "Orders $99 and up ship free!";
-var minSubtotal = 99;
-var widgets;
 
-// Calculating subtotal and displaying the message
-var checkSubtotal = function(order) {
-  if (order) {
-    var subtotal = order.total - order.shipping;
-    if (subtotal > minSubtotal) {
-      alert(promoMessage);
-    }  
-  }
-}
-
-// Detecting whether we're on the cart page and get the cart info
-Ecwid.OnPageLoaded.add(function(page) {
-  widgets = Ecwid.getInitializedWidgets();
-
-  // if storefront widget is present on this page
-  for (i = 0; i < widgets.length ; i++) {
-    if (widgets[i] == "ProductBrowser") {
-      if ('CART' == page.type) {
-        Ecwid.Cart.calculateTotal(function(order) {
-          checkSubtotal(order);
-        });
-      }
-    }
-  }
-});
-
-// Get color value for the message and store it in color variable
-var color = Ecwid.getAppPublicConfig(appId);
 
 // your code here
 // ...
